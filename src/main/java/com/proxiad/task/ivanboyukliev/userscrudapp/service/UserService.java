9a2627bff44adb980e6efa7f32c6df71bc9e5b35
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-25 17:03:40 +0200

    Re-factored parameters of update interface method.

--- src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserService.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserService.java
@@ -1,14 +1,18 @@
 package com.proxiad.task.ivanboyukliev.userscrudapp.service;
 
 import java.util.List;
+import java.util.Optional;
 import com.proxiad.task.ivanboyukliev.userscrudapp.domain.User;
 
 public interface UserService {
   List<User> findAllUsers();
 
+  Optional<User> findUserById(int userId);
+
   void saveUser(User newUser);
 
-  void updateUser(User userForAupdate, User newUserData);
+  void updateUser(int userId, User newUserData);
 
   void deleteUserById(int userId);
+
 }
