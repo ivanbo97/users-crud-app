ffba3471437f364854d8a1438ddb435ced419dd1
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-25 15:28:00 +0200

    Re-factoring related to core changes in repository type.

--- src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserServiceImpl.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.proxiad.task.ivanboyukliev.userscrudapp.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import com.proxiad.task.ivanboyukliev.userscrudapp.domain.User;
 import com.proxiad.task.ivanboyukliev.userscrudapp.repository.UserRepository;
+import com.proxiad.task.ivanboyukliev.userscrudapp.utils.SortUsersByIdAscending;
 
 public class UserServiceImpl implements UserService {
 
@@ -15,7 +19,10 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public List<User> findAllUsers() {
-    return userRepository.findAll();
+    Set<User> currentUsers = userRepository.findAll();
+    List<User> sortedUsers = new ArrayList<>(currentUsers);
+    Collections.sort(sortedUsers, new SortUsersByIdAscending());
+    return sortedUsers;
   }
 
   @Override
@@ -25,7 +32,7 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public void updateUser(User userForAupdate, User newUserData) {
-    userRepository.updateUser(userForAupdate, newUserData);
+    // userRepository.updateUser(userForAupdate, newUserData);
   }
 
   @Override
