8edd5865fd426cd33bf3e2802733df66252351d2
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-25 17:02:00 +0200

    Servlet that redirects to edit user info page.

---
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/servlet/UpdateUserRequestServlet.java
@@ -0,0 +1,34 @@
+package com.proxiad.task.ivanboyukliev.userscrudapp.servlet;
+
+import java.io.IOException;
+import java.util.Optional;
+import com.proxiad.task.ivanboyukliev.userscrudapp.domain.User;
+import com.proxiad.task.ivanboyukliev.userscrudapp.service.UserService;
+import com.proxiad.task.ivanboyukliev.userscrudapp.service.UserServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/updateRequest")
+public class UpdateUserRequestServlet extends HttpServlet {
+
+  private UserService userService = new UserServiceImpl();
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    int id = Integer.parseInt(req.getParameter("id"));
+
+    Optional<User> retrievedUser = userService.findUserById(id);
+    if (retrievedUser.isEmpty()) {
+      // TODO return error jsp page
+      return;
+    }
+
+    getServletContext().setAttribute("user", retrievedUser.get());
+    getServletContext().getRequestDispatcher("/editUser.jsp").forward(req, resp);
+  }
+
+}
