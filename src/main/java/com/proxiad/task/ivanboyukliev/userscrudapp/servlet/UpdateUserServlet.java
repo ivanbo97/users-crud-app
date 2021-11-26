e1e8c651481d20bcf434325d69661f564671b7ba
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-26 17:11:41 +0200

    Inform client if the user was deleted from someone during the editing process.

--- src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/servlet/UpdateUserServlet.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/servlet/UpdateUserServlet.java
@@ -1,15 +1,18 @@
 package com.proxiad.task.ivanboyukliev.userscrudapp.servlet;
 
+import static com.proxiad.task.ivanboyukliev.userscrudapp.utils.ApplicationConstants.UPDATE_USR_URL;
 import java.io.IOException;
+import java.util.Optional;
 import com.proxiad.task.ivanboyukliev.userscrudapp.domain.User;
 import com.proxiad.task.ivanboyukliev.userscrudapp.service.UserService;
+import com.proxiad.task.ivanboyukliev.userscrudapp.utils.HtmlErrorGenerator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/updateUser")
+@WebServlet(UPDATE_USR_URL)
 public class UpdateUserServlet extends HttpServlet {
 
   private static final long serialVersionUID = 345232423L;
@@ -29,7 +32,13 @@ public class UpdateUserServlet extends HttpServlet {
     String newFirstName = req.getParameter("fname");
     String newLastName = req.getParameter("lname");
     User newUserData = new User(userId, newFirstName, newLastName);
-    userService.updateUser(userId, newUserData);
+    Optional<User> updatedUser = userService.updateUser(userId, newUserData);
+
+    if (updatedUser.isEmpty()) {
+      // In case of user being deleted by another client during filling new data
+      resp.getWriter().write(HtmlErrorGenerator.generateNotExisitngStudentErr());
+      return;
+    }
 
     // Update user data in servlet context and redirect to users list
     getServletContext().setAttribute("users", userService.findAllUsers());
