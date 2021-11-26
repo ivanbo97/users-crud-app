e7b58a58689d008a2478c80ddd4d51f2e7c9f56c
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-26 17:05:10 +0200

    Added class for storing app constants. (like URLs)

---
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/utils/ApplicationConstants.java
@@ -0,0 +1,12 @@
+package com.proxiad.task.ivanboyukliev.userscrudapp.utils;
+
+public final class ApplicationConstants {
+
+  // APP End-points
+  public static final String GET_ALL_USRS_URL = "/getAllUsrs";
+  public static final String DELETE_USR_URL = "/delete";
+  public static final String INSERT_USR_URL = "/insertUser";
+  public static final String UPDATE_USR_REQ = "/updateRequest";
+  public static final String UPDATE_USR_URL = "/updateUser";
+
+}
