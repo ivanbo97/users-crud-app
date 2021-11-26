85b5053c35608c9ca54a10de49fc2ee5dcaf6cc1
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-11-26 12:02:19 +0200

    Inject user service from servlet context.

--- src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/servlet/DeleteUserServlet.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/servlet/DeleteUserServlet.java
@@ -2,7 +2,6 @@ package com.proxiad.task.ivanboyukliev.userscrudapp.servlet;
 
 import java.io.IOException;
 import com.proxiad.task.ivanboyukliev.userscrudapp.service.UserService;
-import com.proxiad.task.ivanboyukliev.userscrudapp.service.UserServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,12 +13,18 @@ public class DeleteUserServlet extends HttpServlet {
 
   private static final long serialVersionUID = 83727842346L;
 
-  private UserService userService = new UserServiceImpl();
+  private UserService userService;
+
+  @Override
+  public void init() throws ServletException {
+    userService = (UserService) getServletContext().getAttribute("userService");
+  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     int id = Integer.parseInt(req.getParameter("id"));
+
     userService.deleteUserById(id);
     getServletContext().setAttribute("users", userService.findAllUsers());
     getServletContext().getRequestDispatcher("/viewUsers.jsp").forward(req, resp);
