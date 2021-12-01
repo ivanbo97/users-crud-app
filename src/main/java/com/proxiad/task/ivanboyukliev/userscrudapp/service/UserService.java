d6a269df189d5896776b9774827d0c89bee5445b
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-01 13:48:35 +0200

    Added exception thrown by validator.

--- src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserService.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/service/UserService.java
@@ -3,15 +3,16 @@ package com.proxiad.task.ivanboyukliev.userscrudapp.service;
 import java.util.List;
 import java.util.Optional;
 import com.proxiad.task.ivanboyukliev.userscrudapp.domain.User;
+import com.proxiad.task.ivanboyukliev.userscrudapp.exception.EntityValidationException;
 
 public interface UserService {
   List<User> findAllUsers();
 
   Optional<User> findUserById(int userId);
 
-  void saveUser(User newUser);
+  void saveUser(User newUser) throws EntityValidationException;
 
-  Optional<User> updateUser(int userId, User newUserData);
+  Optional<User> updateUser(int userId, User newUserData) throws EntityValidationException;
 
   void deleteUserById(int userId);
 
