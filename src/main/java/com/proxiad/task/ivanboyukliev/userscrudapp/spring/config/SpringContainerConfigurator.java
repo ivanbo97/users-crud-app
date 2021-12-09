9dd6a181788d8e544641fb17a9c3e352fe0ed8bb
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-09 16:52:59 +0200

    Class for configuration of spring container (with annotations).

---
+++ src/main/java/com/proxiad/task/ivanboyukliev/userscrudapp/spring/config/SpringContainerConfigurator.java
@@ -0,0 +1,10 @@
+package com.proxiad.task.ivanboyukliev.userscrudapp.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.proxiad.task.ivanboyukliev.userscrudapp")
+public class SpringContainerConfigurator {
+
+}
